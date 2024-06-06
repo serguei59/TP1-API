54c15bf7261f6d38f78c1be6ea6cc71e4642e82b
Serge Buasa <sergebuasa@gmail.com>
2024-06-06 15:42:48 +0200

    requete2

--- api_example.py
+++ api_example.py
@@ -1,6 +1,6 @@
 import requests
 
-api_key = '4a7830559ab8bcc5272dfc0ecfb2db8d'
+""" api_key = '4a7830559ab8bcc5272dfc0ecfb2db8d'
 city = 'Paris'
 url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
 
@@ -9,4 +9,44 @@ if response.status_code == 200:
     data = response.json()
     print(data)
 else:
-    print('Erreur:', response.status_code)
+    print('Erreur:', response.status_code) """
+
+def requete1():
+    api_key = '4a7830559ab8bcc5272dfc0ecfb2db8d'
+    city = 'Paris'
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        print(data)
+    else:
+        print('Erreur:', response.status_code)
+    
+#requete1()
+
+def requete2():
+    api_key = '4a7830559ab8bcc5272dfc0ecfb2db8d'
+    city = 'Paris'
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        main = data['main']
+        wind = data['wind']
+        weather = data['weather'][0]
+
+        
+        print(f"Ville: {city}")
+        print(f"Température: {main['temp']}°K")
+        print(f"Pression: {main['pressure']} hPa")
+        print(f"Humidité: {main['humidity']}%")
+        print(f"Vitesse du vent: {wind['speed']} m/s")
+        print(f"Description: {weather['description']}")
+    else:
+        print('Erreur:', response.status_code)
+
+requete2()
+
+
